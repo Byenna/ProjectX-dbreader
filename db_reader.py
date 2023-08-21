9ce321ec3f37978330d571ad09d39e3f259ef968
Byenna <72694655+Byenna@users.noreply.github.com>
2023-08-21 16:43:17 +0200

    added functions

--- db_reader.py
+++ db_reader.py
@@ -1,32 +1,54 @@
+import time
+import requests
 import firebase_admin
 from firebase_admin import credentials, db
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate('path/to/your/serviceAccountKey.json')
+cred = credentials.Certificate('test-12-12-12-firebase-adminsdk-d0rcs-be2e6eb263.json')
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://your-database-url.firebaseio.com'
+    'databaseURL': 'https://test-12-12-12-default-rtdb.europe-west1.firebasedatabase.app/'
 })
 
+# Firebase Realtime Database reference
+ref = db.reference('/page_loads')
+
 # Define your thresholds
-threshold1 = 50
-threshold2 = 100
+page_load_threshold = 0  # in seconds
+server_response_threshold = 200  # in milliseconds
 
-# Reference to the database
-ref = db.reference('/path/to/data')
+def measure_page_load_time(url):
+    start_time = time.time()
+    response = requests.get(url)
+    end_time = time.time()
+    return end_time - start_time
 
-def check_thresholds(data):
-    value = data['value']  # Adjust this based on your database structure
-    if value >= threshold1:
-        print(f"Threshold 1 reached: {value}")
-        # Perform actions when threshold 1 is reached
-    if value >= threshold2:
-        print(f"Threshold 2 reached: {value}")
-        # Perform actions when threshold 2 is reached
+def measure_server_response_time(url):
+    start_time = time.time()
+    response = requests.get(url)
+    end_time = time.time()
+    return (end_time - start_time) * 1000  # Convert to milliseconds
 
-def listen_for_changes(event):
-    if event.event_type == 'put':
-        data = event.data
-        check_thresholds(data)
+def main():
+    url = 'https://google.com'  # Replace with your target URL
 
-# Listen for changes in the database
-ref.listen(listen_for_changes)
+    while True:
+        page_load_time = measure_page_load_time(url)
+        server_response_time = measure_server_response_time(url)
+
+        data = {
+            'timestamp': int(time.time()),
+            'page_load_time': page_load_time,
+            'server_response_time': server_response_time,
+            'url': url
+        }
+        ref.push(data)
+
+        if page_load_time > page_load_threshold:
+            print("Page load time threshold met - page load time:", page_load_time)
+        if server_response_time > server_response_threshold:
+            print("Server response time threshold met - server response time:", server_response_time)
+
+        time.sleep(60)  # Wait for 1 minute before measuring again
+
+if __name__ == "__main__":
+    main()
